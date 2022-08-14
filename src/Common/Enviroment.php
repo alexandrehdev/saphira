1fc68f50dc1b172600985faadfcef3053dfbf674
alexandre <hallyssonrdev@gmail.com>
2022-08-14 17:48:09 -0300

    Update Enviroment.php

--- src/Common/Enviroment.php
+++ src/Common/Enviroment.php
@@ -4,25 +4,21 @@ namespace Saphira\Connectdb\Common;
 class Enviroment
 {
 
-	public static function load($dir){
-    $pathEnv =  "/vendor/saphira/connectdb/.env";
+ public static function load($dir){
+        $rootFolder = $_SERVER["DOCUMENT_ROOT"];
+        $pathEnv =  "/vendor/saphira/connectdb/.env";
 
-    for($x = 1; $x <= 20; $x++){
-      if(dirname($dir,$x) == dirname(getcwd(),1)){
-        $rootFolder = dirname($dir,$x);
-        $lines      = (file_exists($rootFolder . $pathEnv)) ? file($rootFolder . $pathEnv) : null;
+        for($x = 1; $x <= 20; $x++){
+            if($rootFolder == dirname($dir,$x)){
+              $lines = (file_exists($rootFolder . $pathEnv)) ? file($rootFolder . $pathEnv) : null;
+              foreach($lines as $line){
+                putenv(trim($line));
+              }
+                return ".env file 200";
 
-          foreach($lines as $line){
-            
-            putenv(trim($line));
-
-          }
-
-        return ".env file 200";
-
-        continue;
-      }
+                continue;
+            }
+        }
     }
-
 	}
 }

9cfb4dafbc948a0f9ad32f436816426b7b7eb1df
alexandre <hallyssonrdev@gmail.com>
2022-08-14 18:07:21 -0300

    Update Connection.php

--- src/Connect/Connection.php
+++ src/Connect/Connection.php
@@ -6,46 +6,24 @@ use PDOException;
 
 class Connection{
 
-	/**
-	 * variable that store the connection
-	 *
-	 * @var object
-	 */
 
 	private $con;
 
 	
-	/**
-	 * set connection
-	 *
-	 * @param object $data
-	 * @return void
-	 */
 	public function setCon($data){
 		$this->con = $data;
 	}
 
-	/**
-	 * return connection
-	 *
-	 * @return object
-	 */
 	public function getCon(){
-		return $this->con;
+	   return $this->con;
 	}
 
-
-
  	function __construct(){
-		
- 		Enviroment::load(__DIR__);
-
 	    try{
-
-		    $server = getenv("DB_HOST");
-		    $user	= getenv("DB_USER");
-		    $bd 	= getenv("DB_NAME");
-	        $pwd 	= getenv("DB_PASS");
+	        $server = getenv("DB_HOST");
+		$user = getenv("DB_USER");
+		$bd = getenv("DB_NAME");
+	        $pwd = getenv("DB_PASS");
 		    $this->setCon(new PDO("mysql:host=$server;dbname=$bd",$user,$pwd));
 	        $this->con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
@@ -54,4 +32,4 @@ class Connection{
 	   }
   }
 }
- ?>
+

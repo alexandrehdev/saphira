b452fca5904e82d77f69897fd61971df877c24b9
alexandre <hallyssonrdev@gmail.com>
2022-09-15 18:50:44 -0300

    Update Enviroment.php

--- src/Common/Enviroment.php
+++ src/Common/Enviroment.php
@@ -4,22 +4,33 @@ namespace Saphira\Connectdb\Common;
 class Enviroment
 {
 
+   const SUCCESS = 'Enviroment File Was Found';
+
+    const FAIL = 'Enviroment File Not Found';
+
+
     public static function load($dir){
-
-       $fileEnv =  "/.env";
-       if(file_exists($dir . $fileEnv)){
-
-          $lines = file($dir . $fileEnv);
-          foreach($lines as $line){
-              putenv(trim($line));
-          }
-
-          return ".env 200";
-
-       }else{
-          return ".env file not found";
-       }
-
+        $rootPath = $_SERVER['DOCUMENT_ROOT'];
+        $fileEnv =  "/.env";
+            for($x = 1; $x <= 40; $x++){
+                if(dirname($dir,$x) == $rootPath){
+                    $rootFolder = dirname($dir,$x);
+                    if(file_exists($rootFolder . $fileEnv)){
+                        $envs = file($rootFolder . $fileEnv);
+                            foreach($envs as $env){
+                                putenv(trim($env));
+                            }                     
+                       return Enviroment::SUCCESS;                
+                    }else{ 
+                       return Enviroment::FAIL;
+                  }
+              }
+      }
+        
+        
    }
+
+
+
 
  }

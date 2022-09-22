cba9e089edfd295226e2acd851c0c389b3e1ad0b
alexandrehdev <hallyssonrdev@gmail.com>
2022-09-22 17:28:00 -0300

    better loading enviroment

--- src/Common/Enviroment.php
+++ src/Common/Enviroment.php
@@ -1,36 +1,36 @@
 <?php
 namespace Saphira\Connectdb\Common;
 
+use Exception;
+
 class Enviroment
 {
+    
+    private $env;
 
-   const SUCCESS = 'Enviroment File Was Found';
 
-    const FAIL = 'Enviroment File Not Found';
+
+    public function __construct(){
+        $this->env = "/.env";
+    }
+
 
 
     public static function load($dir){
-        $rootPath = $_SERVER['DOCUMENT_ROOT'];
-        $fileEnv =  "/.env";
-            for($x = 1; $x <= 40; $x++){
-                if(dirname($dir,$x) == $rootPath){
-                    $rootFolder = dirname($dir,$x);
-                    if(file_exists($rootFolder . $fileEnv)){
-                        $envs = file($rootFolder . $fileEnv);
-                            foreach($envs as $env){
-                                putenv(trim($env));
-                            }                     
-                       return Enviroment::SUCCESS;                
-                    }else{ 
-                       return Enviroment::FAIL;
-                  }
-              }
-      }
+       $self = new Static;
+       $searchEnv = file_exists($dir . $self->env);
+       $envs = ($searchEnv) ? file($dir . $self->env) : false;
+
+       foreach($envs as $env){
+          $response = (!empty($env) ? putenv($env) : false);
+       }
+
+       return $response;
+    }
         
         
-   }
+}
 
 
 
 
- }
